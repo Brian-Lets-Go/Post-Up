88c497580f4b503821276792ce770a382b903f87
jAmWhisk <merrychu1005@gmail.com>
2022-03-28 02:12:17 -0400

    Add user handlebars content, add dashboard to home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,30 @@
 const router = require('express').Router();
 
+
 router.get('/', (req, res) => {
     res.render('homepage');
 });
 
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
 
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+
     res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.signedUp) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('signup');
 });
 
 module.exports = router;
