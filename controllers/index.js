8d8ace600b8ed556bb32726d80e2e33b3f7c0fd3
jAmWhisk <merrychu1005@gmail.com>
2022-03-28 01:30:24 -0400

    add content to dashboard handlebar and add partial for that

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
 const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
