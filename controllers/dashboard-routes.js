1f0e7e80e1872819882b726be644eab965541921
Sarah Hart <sarah.hart.nc@gmail.com>
2022-03-28 14:44:26 -0400

    update get route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,8 +3,43 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('dashboard', { loggedIn: true });
-  });
+    Post.findAll({
+        where: {
+          // use the ID from the session
+          user_id: req.session.user_id
+        },
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+          // serialize data before passing to template
+          const posts = dbPostData.map(post => post.get({ plain: true }));
+          res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
 
 module.exports = router;
 
