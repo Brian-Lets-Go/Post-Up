eb6a37dabed3ce97c8c711676f778fcee7b3bd22
Mohammad Rahman <mbrahman@gmail.com>
2022-03-29 19:46:45 -0400

    added cloud sql

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// create connection to our db
+const Sequelize = require('sequelize');
+
+const fs = require('fs')
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+    host: 'db.stippled.art',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+      ssl: {
+        key: fs.readFileSync('./certs/client-key.cer'),
+        cert: fs.readFileSync('./certs/client-cert.cer'),
+        ca: fs.readFileSync('./certs/server-ca.cer')
+      },
+    },
+  });
 
 module.exports = sequelize;
